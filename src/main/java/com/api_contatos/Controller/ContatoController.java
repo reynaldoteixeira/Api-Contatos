11f52b51bee9dd280369d32312c7b4522f10a3ac
ReynaldoTeixeira <reynaldoteixeira@gmail.com>
2019-03-12 23:21:30 -0300

    Configurando ContatoRepository para o Controller

--- src/main/java/com/api_contatos/Controller/ContatoController.java
+++ src/main/java/com/api_contatos/Controller/ContatoController.java
@@ -1,9 +1,15 @@
 package com.api_contatos.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.api_contatos.repository.ContatoRepository;
 
 @Controller
 @RequestMapping(path="contatos")
 public class ContatoController {
-
+	
+	@Autowired
+	private ContatoRepository cr;
 }
