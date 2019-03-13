2ae4400cfeb6d5c19fd7c177ea45069bf84b1dc0
ReynaldoTeixeira <reynaldoteixeira@gmail.com>
2019-03-12 23:06:47 -0300

    Configurando atributos na classe 'model'

--- src/main/java/com/api_contatos/model/Contato.java
+++ src/main/java/com/api_contatos/model/Contato.java
@@ -1,5 +1,15 @@
 package com.api_contatos.model;
 
-public class Contato {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Contato {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String nome;
+	private int numero;
 }
