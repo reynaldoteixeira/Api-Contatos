b32952b9f8e2e8600f77327c4a9e5f34b30bb82a
ReynaldoTeixeira <reynaldoteixeira@gmail.com>
2019-03-12 22:39:34 -0300

    Inicialização projeto

---
+++ src/test/java/com/api_contatos/ApiContatosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.api_contatos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiContatosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
