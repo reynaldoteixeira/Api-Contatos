49189f935ab4a2f302ac1ba79436c8a9c70ee427
ReynaldoTeixeira <reynaldoteixeira@gmail.com>
2019-03-13 11:56:12 -0300

    Criando método delete - Controller

--- src/main/java/com/api_contatos/Controller/ContatoController.java
+++ src/main/java/com/api_contatos/Controller/ContatoController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.api_contatos.model.Contato;
 import com.api_contatos.repository.ContatoRepository;
+
 
 
 
@@ -31,5 +33,11 @@ public class ContatoController {
 	@PostMapping()
 	public Contato cadastroContato(@RequestBody @Valid Contato contato) {
 		return cr.save(contato);
+	}
+	
+	@DeleteMapping()
+	public Contato deletaContato(@RequestBody Contato contato) {
+		cr.delete(contato);
+		return contato;
 	}
 }
