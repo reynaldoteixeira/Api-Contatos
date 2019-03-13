cb58db101fccc5154b2d61f013463f8c798bb466
ReynaldoTeixeira <reynaldoteixeira@gmail.com>
2019-03-12 23:08:59 -0300

    Add Getters e Setters dos atributos na classe 'model'

--- src/main/java/com/api_contatos/model/Contato.java
+++ src/main/java/com/api_contatos/model/Contato.java
@@ -10,6 +10,28 @@ public class Contato {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	
 	private String nome;
+	
 	private int numero;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
 }
